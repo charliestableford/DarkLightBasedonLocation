2595ac9bbc06fa7875f77f99116c500fcab6c9f6
Charlie Stableford <charliestableford2@gmail.com>
2021-01-02 18:04:39 -0800

    working

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const app = () => {
     const endpoint = 'https://api.sunrise-sunset.org/json?lat=53.7267&lng=-127.6476&date=today';
 
-     const getLocation = async () => {
+     return getLocation = async () => {
         const response = await fetch(endpoint)
         const data = await response.json();
         let body = document.querySelector("body");
         let current = new Date();
         let time = (current.getHours()>=17) ? "Evening" : "Daytime";
         console.log(`this is the time: ${time}`);
+
         let sunset = data.results.sunset;
         let sunrise = data.results.sunrise;
-          console.log(`sunset: ${sunset}`);
-          console.log(`sunrise: ${sunrise}`);
+
+        console.log(`sunset: ${sunset}`);
+        console.log(`sunrise: ${sunrise}`);
         
         // if time is evening then set to sunset, else it is sunrise
         let background = (time === "Evening") ? body = body.style.background = "black" : body = body.style.background = "white"  ;
@@ -21,5 +23,6 @@ const app = () => {
 }
 
 window.addEventListener('load', (event) => {
-    app();
+    app(); 
+    getLocation();
   });

096c237591459446d2ed2c7c6b98b4098ec239df
Charlie Stableford <charliestableford2@gmail.com>
2021-01-02 21:04:23 -0800

    time

--- script.js
+++ script.js
@@ -12,28 +12,29 @@ const app = () => {
         let sunset = data.results.sunset;
         let sunrise = data.results.sunrise;
 
+        let currentHours = current.getHours();
+        let currentMinutes = current.getMinutes();
+        console.log(`${currentHours}:${currentMinutes}`);
+
         console.log(`sunset: ${sunset}`);
         console.log(`sunrise: ${sunrise}`);
         
         // if time is evening then set to sunset, else it is sunrise
         let background = (time === "Evening") ? body = body.style.background = "black" : body = body.style.background = "white"  ;
         
-        // return timeIcon = () => {
-          let sunsetImg = new Image(50, 50);
-          sunsetImg.src = 'assets/sunset.png';
-          document.body.appendChild(sunsetImg);
-          sunsetImg.setAttribute("alt", "Image icon of a moon.");
+        let sunsetImg = new Image(50, 50);
+        sunsetImg.src = 'assets/sunset.png';
+        document.body.appendChild(sunsetImg);
+        sunsetImg.setAttribute("alt", "Image icon of a moon.");
+        sunsetImg.setAttribute("class", "sunsetImg");
 
-          let sunriseImg = new Image(60, 50);
-          sunriseImg.src = 'assets/sunrise.png';
-          document.body.appendChild(sunriseImg);
-          sunriseImg.setAttribute("alt", "Image icon of the sun rising.");
+        let sunriseImg = new Image(60, 50);
+        sunriseImg.src = 'assets/sunrise.png';
+        document.body.appendChild(sunriseImg);
+        sunriseImg.setAttribute("alt", "Image icon of the sun rising.");
+        sunriseImg.setAttribute("class", "sunriseImg");
 
-          let icon = (time === "Evening") ? (sunsetImg.style.display = "block", sunriseImg.style.display = "none") : (sunriseImg.style.display = "block", sunsetImg.style.display = "none");
-          console.log(icon);
-
-
-        // }
+        let icon = (time === "Evening") ? (sunsetImg.style.display = "block", sunriseImg.style.display = "none") : (sunriseImg.style.display = "block", sunsetImg.style.display = "none");
 
         return data;
   } 
@@ -42,5 +43,4 @@ const app = () => {
 window.addEventListener('load', (event) => {
     app(); 
     getLocation();
-    // timeIcon();
   });

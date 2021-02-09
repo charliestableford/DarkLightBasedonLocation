e721ca754f3809683604f874c5802488a68f7755
Charlie Stableford <charliestableford2@gmail.com>
2021-02-09 10:35:56 -0800

    sizing

--- script.js
+++ script.js
@@ -21,29 +21,34 @@ const app = () => {
         let currentMinutes = current.getMinutes();
         console.log(`${currentHours}:${currentMinutes}`);
 
-        let currentTime = document.createElement('div');
-        currentTime.appendChild(document.createTextNode(`${currentHours}:${currentMinutes}`));
-        document.body.appendChild(currentTime);
-        currentTime.setAttribute("class", "currentTime");
+        let currentTime = document.querySelector('.currentTime');
+        let timeIconText = document.querySelector('.timeIconText');
+        let sunsetImg = document.querySelector('.sunsetImg');
+        let sunriseImg = document.querySelector('.sunriseImg');
+
+        // let currentTime = document.createElement('div');
+        // currentTime.appendChild(document.createTextNode(`${currentHours}:${currentMinutes}`));
+        // document.body.appendChild(currentTime);
+        // currentTime.setAttribute("class", "currentTime");
         
         let background = (time === "Evening") ? (body.style.background = "black", body.style.color = "white", currentTime.style.color="white") : (body.style.background = "white", body.style.color = "black", currentTime.style.color="black");
 
-        let timeIconText = document.createElement('div');
-        timeIconText.appendChild(document.createTextNode('Currently residing in Vancouver, BC, Canada'));
-        document.body.appendChild(timeIconText);
-        timeIconText.setAttribute("class", "timeIconText");
+        // let timeIconText = document.createElement('div');
+        // timeIconText.appendChild(document.createTextNode('Currently residing in Vancouver, BC, Canada'));
+        // document.body.appendChild(timeIconText);
+        // timeIconText.setAttribute("class", "timeIconText");
         
-        let sunsetImg = new Image(50, 50);
-        sunsetImg.src = 'assets/sunset.png';
-        document.body.appendChild(sunsetImg);
-        sunsetImg.setAttribute("alt", "Image icon of a moon.");
-        sunsetImg.setAttribute("class", "sunsetImg");
+        // let sunsetImg = new Image(50, 50);
+        // sunsetImg.src = 'assets/sunset.png';
+        // document.body.appendChild(sunsetImg);
+        // sunsetImg.setAttribute("alt", "Image icon of a moon.");
+        // sunsetImg.setAttribute("class", "sunsetImg");
 
-        let sunriseImg = new Image(60, 50);
-        sunriseImg.src = 'assets/sunrise.png';
-        document.body.appendChild(sunriseImg);
-        sunriseImg.setAttribute("alt", "Image icon of the sun rising.");
-        sunriseImg.setAttribute("class", "sunriseImg");
+        // let sunriseImg = new Image(60, 50);
+        // sunriseImg.src = 'assets/sunrise.png';
+        // document.body.appendChild(sunriseImg);
+        // sunriseImg.setAttribute("alt", "Image icon of the sun rising.");
+        // sunriseImg.setAttribute("class", "sunriseImg");
 
         let icon = (time === "Evening") ? (sunsetImg.style.display = "block", sunriseImg.style.display = "none") : (sunriseImg.style.display = "block", sunsetImg.style.display = "none");
 
